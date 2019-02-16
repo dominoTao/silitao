40c539b7ead8fa7778223fcc8653e75988439174
dominoTao <idiotao@163.com>
2019-02-16 19:54:38 +0800

    构造数组的MaxTree完成

--- src/main/java/com/liangtao/core/stack/MaxTree.java
+++ src/main/java/com/liangtao/core/stack/MaxTree.java
@@ -14,12 +14,77 @@ import java.util.Stack;
  *
  */
 public class MaxTree {
+	public static void main(String[] args) {
+		MaxTree mt = new MaxTree();
+		int[] arr = {2,3,4,1,6,55,34,23,12,123,1233};
+		System.out.println(mt.getMaxTree(arr).value);
+		System.out.println(mt.getMaxTree(arr).left.value);
+		System.out.println(mt.getMaxTree(arr).right.value);
+	}
 	public Node getMaxTree(int[] arr) {
+		//创建一个arr数组相同长度的Node类型的数组
 		Node[] nArr = new Node[arr.length];
+		//将arr中的值依次存入nArr数组中
 		for(int i = 0; i != arr.length; i++) {
 			nArr[i] = new Node(arr[i]);
 		}
-		return null;
+		Stack<Node> stack = new Stack<Node>();
+		HashMap<Node, Node> lBigMap = new HashMap<Node, Node>();
+		HashMap<Node, Node> rBigMap = new HashMap<Node, Node>();
+		//看不懂的可以看看StackSort类
+		//lBigMap中完成存储value是key左侧第一个比它大的元素
+		for(int i = 0; i != nArr.length; i++) {
+			Node curNode = nArr[i];
+			while((!stack.isEmpty()) && stack.peek().value < curNode.value) {
+				popStackSetMap(stack, lBigMap);
+			}
+			stack.push(curNode);
+		}
+		while(!stack.isEmpty()) {
+			popStackSetMap(stack, lBigMap);
+		}
+		//rBigMap中完成存储value是key右侧第一个比它大的元素
+		for(int i = nArr.length - 1; i != -1; i--) {
+			Node curNode = nArr[i];
+			while((!stack.isEmpty()) && stack.peek().value < curNode.value) {
+				popStackSetMap(stack, rBigMap);
+			}
+			stack.push(curNode);
+		}
+		while(!stack.isEmpty()) {
+			popStackSetMap(stack, rBigMap);
+		}
+		Node head = null;
+		for(int i = 0; i != nArr.length; i++) {
+			Node curNode = nArr[i];
+			Node left = lBigMap.get(curNode);
+			Node right = rBigMap.get(curNode);
+			//如果一个数左边和右边都没有比它大的数，那么这个数就是整个数组的最大值，即是MaxTree的头结点
+			if(left == null && right == null) {
+				head = curNode;
+			}else if(left == null) {
+				if(right.left == null) {
+					right.left = curNode;
+				}else {
+					right.right = curNode;
+				}
+			}else if(right == null) {
+				if(left.left == null) {
+					left.left = curNode;
+				}else {
+					left.right = curNode;
+				}
+			}else {
+				//每一个数的父节点是它左边第一个比它大的数和它右边第一个比它大的数中，比较小的那个
+				Node parent = left.value < right.value ? left : right;
+				if(parent.left == null) {
+					parent.left = curNode;
+				}else {
+					parent.right = curNode;
+				}
+			}
+		}
+		return head;
 	}
 	
 	public void popStackSetMap(Stack<Node> stack, HashMap<Node, Node> map) {
